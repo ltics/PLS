c14d19afc657c3723b27f2a3a1d0de399bf767ee
zjhmale <zjhmale@gmail.com>
2015-10-02 13:09:20 +0800

    finish forth lang repl

---
+++ clojure-pl/src/clojure_pl/core.clj
@@ -0,0 +1,10 @@
+(ns clojure-pl.core
+  (:require [clojure-pl.forth.forth :refer [init-struct] :as forth])
+  (:gen-class))
+
+(defn -main
+  [& args]
+  (if args
+    (condp = (first args)
+      "forth" (do (println "mini forth lang =>")
+                  (forth/repl (init-struct))))))
---
+++ clojure-pl/src/clojure_pl/forth/forth.clj
@@ -0,0 +1,70 @@
+(ns clojure-pl.forth.forth
+  (:require [clojure-pl.cota :refer :all])
+  (:import (java.util Scanner))
+  (:refer-clojure :exclude [pop!]))
+
+(declare forth-eval*)
+
+(defn pop!
+  [stack]
+  (let [top-elem (car @stack)]
+    (swap! stack pop)
+    top-elem))
+
+(defn push!
+  [stack item]
+  (swap! stack conj item))
+
+(defn next-token
+  [stream]
+  (if (. stream hasNextInt)
+    (. stream nextInt)
+    (. stream next)))
+
+(defn gen-f
+  [op stack]
+  #(let [a (pop! stack)
+         b (pop! stack)]
+    (push! stack (op b a))))
+
+(defn init-struct []
+  (let [stream (Scanner. System/in)
+        stack (atom '())
+        env (atom {})
+        extend-env (fn [var f] (swap! env assoc var f))]
+    (extend-env ".s" #(do (println (apply str (repeat 6 "-")))
+                          (doseq [elem @stack] (prn elem))
+                          (println (apply str (repeat 6 "-")))))
+    ;;just start a new line
+    (extend-env "cr" #(prn))
+    (extend-env "+" (gen-f + stack))
+    (extend-env "-" (gen-f - stack))
+    (extend-env "*" (gen-f * stack))
+    (extend-env "/" (gen-f / stack))
+    ;;just duplicate the top elem
+    (extend-env "dup" #(push! stack (car @stack)))
+    (extend-env "." #(prn (pop! stack)))
+    (extend-env ":" #(let [name (next-token stream)
+                           block (loop [b []
+                                        n (next-token stream)]
+                                   (if (= n ";")
+                                     b
+                                     (recur (conj b n) (next-token stream))))]
+                      (extend-env name (fn [] (doseq [token block]
+                                                (forth-eval* env stack token))))))
+    [env stack stream]))
+
+(defn forth-eval*
+  [env stack token]
+  (cond
+    (contains? @env token) ((@env token))
+    (number? token) (push! stack token)
+    :else (prn "unknown token " token)))
+
+(defn repl
+  [struct]
+  (let [[env stack stream] struct
+        token (next-token stream)]
+    (when-not (= token "quit")
+      (forth-eval* env stack token)
+      (repl struct))))
