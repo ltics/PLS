efd033feee7bf4674310eae2e24ad7d55431614c
zjhmale <zjhmale@gmail.com>
2015-10-03 14:20:53 +0800

    add colorful test framework

--- clojure-pl/project.clj
+++ clojure-pl/project.clj
@@ -2,7 +2,9 @@
   :description "pls"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.6.0"]]
+  :dependencies [[org.clojure/clojure "1.6.0"]
+                 [acolfut "0.1.0"]]
+  :plugins [[colortest "0.1.0"]]
   :main ^:skip-aot clojure-pl.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}})
