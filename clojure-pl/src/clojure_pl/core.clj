ca228530a5123722562ca1c2dd164d9fc04c73f7
zjhmale <zjhmale@gmail.com>
2015-10-07 00:55:43 +0800

    implement repl

--- clojure-pl/src/clojure_pl/core.clj
+++ clojure-pl/src/clojure_pl/core.clj
@@ -1,13 +1,18 @@
 (ns clojure-pl.core
-  (:require [clojure-pl.bf.bf :refer [run] :as bf]
+  (:require [clojure-pl.cota :refer :all]
+            [clojure-pl.bf.bf :refer [run] :as bf]
             [clojure-pl.forth.forth :refer [init-struct] :as forth]
-            [clojure-pl.lsystem.lsystem :refer [draw-dragon draw-pentigree draw-triangle]])
+            [clojure-pl.lsystem.lsystem :refer [draw-dragon draw-pentigree draw-triangle]]
+            [clojure-pl.lisp.interpreter.repl :as lisp])
   (:gen-class))
+
+(reset! *debug* false)
 
 (defn -main
   [& args]
   (if args
     (condp = (first args)
+      "lisp" (lisp/repl)
       "bf" (let [filepath (second args)]
              (bf/run filepath))
       "forth" (do (println "mini forth lang =>")
--- clojure-pl/src/clojure_pl/lisp/interpreter/repl.clj
+++ clojure-pl/src/clojure_pl/lisp/interpreter/repl.clj
@@ -4,5 +4,21 @@
             [clojure-pl.lisp.interpreter.interp :refer :all])
   (:refer-clojure :exclude [list*]))
 
-(defn repl
-  [])
+(defn repl*
+  {:private true}
+  [[res env]]
+  (println res)
+  (print "=> ")
+  (flush)
+  (if-let [line (read-line)]
+    (recur (eval* (parse line) env))))
+
+(defn repl []
+  (println "lisp interpreter")
+  (print "=> ")
+  (flush)
+  (if-let [line (read-line)]
+    (-> line
+        parse
+        (eval* buildin-env)
+        repl*)))
