07a8ddb4552de767ba1648f3e5b462af2c3264e5
zjhmale <zjhmale@gmail.com>
2015-10-07 16:40:04 +0800

    finish core funcs of metainterp

---
+++ clojure-pl/src/clojure_pl/lisp/metainterp/meta.clj
@@ -0,0 +1,46 @@
+(ns clojure-pl.lisp.metainterp.meta
+  (:require [clojure-pl.cota :refer :all])
+  (:refer-clojure :exclude [cond cons let]))
+
+(defmacro cond
+  [& body]
+  (when body
+    (clojure.core/let
+      [[pred# conseq#] (first body)
+       rst# (next body)
+       pred# (if (= pred# 'else)
+               :else pred#)]
+      `(if ~pred#
+         ~conseq#
+         (cond ~@rst#)))))
+
+(defmacro define
+  [name-with-args body]
+  (if (coll? name-with-args)
+    `(defn ~(car name-with-args) [~@(rest name-with-args)] ~body)
+    `(def ~name-with-args ~body)))
+
+(defmacro let
+  [bindings & body]
+  (clojure.core/let [bindings# (reduce concat [] bindings)]
+    `(clojure.core/let [~@bindings#] ~@body)))
+
+(defmacro begin
+  [& body]
+  `(do ~@body))
+
+(defmacro lambda
+  [args body & _]
+  `(fn [~@args] ~body))
+
+(defn cons
+  [fst snd]
+  (clojure.core/cons
+    fst
+    (if (coll? snd)
+      snd
+      [snd])))
+
+(def append concat)
+(def null? nil-or-empty?)
+(def display println)
