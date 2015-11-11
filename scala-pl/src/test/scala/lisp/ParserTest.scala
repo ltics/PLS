fd34f2e85d7912342851604cf62566a37533eccf
zjhmale <zjhmale@gmail.com>
2015-11-11 14:35:05 +0800

    finish parser test

--- scala-pl/src/test/scala/lisp/ParserTest.scala
+++ scala-pl/src/test/scala/lisp/ParserTest.scala
@@ -11,4 +11,12 @@ class ParserTest extends FlatSpec with Matchers {
         parse("(def clea \"clea\")") should be (List(Comb(List(Symbol("def"), Symbol("clea"), Value(Name("clea"))))))
         parse("\"clea\"") should be (List(Value(Name("clea"))))
     }
+
+    it should "parse multiple token without brackets" in {
+        parse("3 3") should be (List(Value(Num(3.0)), Value(Num(3.0))))
+    }
+
+    it should "parse s-expressions" in {
+        parse("(+ 3 3)") should be (List(Comb(List(Symbol("+"), Value(Num(3.0)), Value(Num(3.0))))))
+    }
 }
