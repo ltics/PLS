7988dfdd52c5fcf5286d96675ee7903d7038c78c
zjhmale <zjhmale@gmail.com>
2015-12-21 01:03:25 +0800

    more rules

--- clojure-pl/src/clojure_pl/logic/trs.clj
+++ clojure-pl/src/clojure_pl/logic/trs.clj
@@ -33,7 +33,6 @@
      s#
      (== false q))
    (lazy-seq '(false)))
-
 ;; (== false x) will be succeed depend on the value of x
 
 (let [x true]
@@ -48,7 +47,6 @@
      (let [x true]
        (== true x)))
    (lazy-seq '(_0)))
-
 ;; 因为中间的relation返回的goal是success的
 ;; 而let又是local bind是一个lambda abstraction
 ;; 所以外面的fresh变量可以为任何值 就用占位符代替了
@@ -57,7 +55,6 @@
      (let [x true]
        (== false x)))
    (lazy-seq '()))
-
 ;; 中间的relation的goal是失败 所以外面的fresh变量没有归一值
 
 (= (run* [q]
@@ -65,7 +62,6 @@
        (== true x)
        (== true q)))
    (lazy-seq '(true)))
-
 ;; because ‘(fresh (x ...) g ...)’ binds fresh variables to x ...
 ;; and succeeds if the goals g ... succeed.
 ;; (== v x) succeeds when x is fresh. q, x are all fresh variable
@@ -78,11 +74,41 @@
        (== x true)
        (== q true)))
    (lazy-seq '(true)))
-
 ;; because the order of arguments to == does not matter.
 
 (= (run* [q]
      s#)
    (lazy-seq '(_0)))
-
 ;; value associated with x '_0 is a symbol representing a fresh variable.
+
+(= (run* [x]
+     (let [x false]
+       (fresh [x]
+         (== true x))))
+   (lazy-seq '(_0)))
+;; sincethex in(== true x) is the one introduced by the fresh expression
+;; it is neither the x introduced in the run expression
+;; nor the x introduced in the lambda expression(let).
+
+(= (run* [r]
+     (fresh [x y]
+       (== (cons x (cons y '())) r)))
+   (lazy-seq '((_0 _1))))
+;; For each different fresh variable there is a symbol with an underscore followed by a numeric subscript.
+;; This entity is not a variable but rather is a way of showing that the variable was fresh.
+;; We say that such a variable has been reified.
+
+(= (run* [r]
+     (fresh [x]
+       (let [y x]
+         (fresh [x]
+           (== (cons x (cons y (cons x '()))) r)))))
+   (run* [r]
+     (fresh [x]
+       (let [y x]
+         (fresh [x]
+           (== (cons y (cons x (cons y '()))) r)))))
+   (lazy-seq '((_0 _1 _0))))
+;; Within the inner fresh, x and y are different variables,
+;; and since they are still fresh, they get different reified names.
+;; Reifying r’s value reifies the fresh variables in the order in which they appear in the list.
