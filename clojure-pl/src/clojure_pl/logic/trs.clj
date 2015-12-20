8e45f84064fc24b0264e028d029f9d0252b89e09
zjhmale <zjhmale@gmail.com>
2015-12-20 19:42:22 +0800

    fresh and unification law

--- clojure-pl/src/clojure_pl/logic/trs.clj
+++ clojure-pl/src/clojure_pl/logic/trs.clj
@@ -9,7 +9,8 @@
 (= (run* [q]
      u#)
    (lazy-seq '()))
-;; since u# fails, and because the expression (run∗ (q) g ...) has the value () if any goal in g ... fails.
+;; since u# fails,
+;; and because the expression (run∗ (q) g ...) has the value () if any goal in g ... fails.
 
 (= (run* [q]
      (== s# q))
@@ -27,3 +28,61 @@
      (== true q))
    (lazy-seq '(true)))
 ;; because the expression (run∗ (q) g ... (≡ true q)) associates true with q if the goals g ... and (≡ true q) succeed.
+
+(= (run* [q]
+     s#
+     (== false q))
+   (lazy-seq '(false)))
+
+;; (== false x) will be succeed depend on the value of x
+
+(let [x true]
+  (== false x))
+;; unify will failed case x is not false
+
+(let [x false]
+  (== false x))
+;; unify will success case x is false
+
+(= (run* [x]
+     (let [x true]
+       (== true x)))
+   (lazy-seq '(_0)))
+
+;; 因为中间的relation返回的goal是success的
+;; 而let又是local bind是一个lambda abstraction
+;; 所以外面的fresh变量可以为任何值 就用占位符代替了
+
+(= (run* [x]
+     (let [x true]
+       (== false x)))
+   (lazy-seq '()))
+
+;; 中间的relation的goal是失败 所以外面的fresh变量没有归一值
+
+(= (run* [q]
+     (fresh [x]
+       (== true x)
+       (== true q)))
+   (lazy-seq '(true)))
+
+;; because ‘(fresh (x ...) g ...)’ binds fresh variables to x ...
+;; and succeeds if the goals g ... succeed.
+;; (== v x) succeeds when x is fresh. q, x are all fresh variable
+
+;; the law of fresh => If x is fresh, then (== v x) succeeds and associates x with v.
+;; the law of == (unification) => (≡v w)isthesameas(≡w v).
+
+(= (run* [q]
+     (fresh [x]
+       (== x true)
+       (== q true)))
+   (lazy-seq '(true)))
+
+;; because the order of arguments to == does not matter.
+
+(= (run* [q]
+     s#)
+   (lazy-seq '(_0)))
+
+;; value associated with x '_0 is a symbol representing a fresh variable.
