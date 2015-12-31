23915ae88eaffbb86548e2b75d1862fb9cd8f3dc
zjhmale <zjhmale@gmail.com>
2015-12-31 15:28:14 +0800

    add prolog support

---
+++ prolog-pl/basic1.pl
@@ -0,0 +1,13 @@
+/*facts*/
+likes(dan, sally).
+likes(sally, dan).
+likes(josh, brittney).
+
+/*rules*/
+dating(X, Y) :-
+likes(X, Y),
+likes(Y, X).
+
+friendship(X, Y) :-
+likes(X, Y);
+likes(Y, X).
